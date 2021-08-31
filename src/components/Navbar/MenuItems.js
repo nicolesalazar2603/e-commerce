c4e3cb09396992099db2410aabfbed7ed45d229d
nicolesalazar2603 <72114870+nicolesalazar2603@users.noreply.github.com>
2021-08-31 18:06:25 +0200

    navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Inicio',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contactanos',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Registrate',
+        url: '#',
+        cName: 'nav-links-mobile',
+    },
+]
